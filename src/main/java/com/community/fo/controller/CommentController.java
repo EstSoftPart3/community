1fa7322b8aefa9b06a0eff1242a26dfbaf01e0f0
nsj9024 <nsj9024@gmail.com>
2024-03-12 15:33:57 +0900

    노상준 - 댓글 수정기능 구현 커밋

--- src/main/java/com/community/fo/controller/CommentController.java
+++ src/main/java/com/community/fo/controller/CommentController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,12 +16,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.community.fo.jpa.entity.BoardEntity;
 import com.community.fo.jpa.entity.CommentEntity;
 import com.community.fo.service.CommentServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 @RequestMapping
 public class CommentController {
@@ -27,6 +30,7 @@ public class CommentController {
 	private final CommentServiceImpl csi;
 	
 	@GetMapping("/comments")
+	@ResponseBody
 	public List<CommentEntity> comments () {
 		
 		 List<CommentEntity> comments = csi.commentList();
@@ -35,14 +39,34 @@ public class CommentController {
 	
 	
 	@DeleteMapping("/{commentSq}")
+	@ResponseBody
 	public void deleteComment(@RequestParam("commentSq") int commentSq) {
 		
 		csi.CommentDelete(commentSq);
 
 	
 	}
+	@GetMapping("/comment/{commentSq}")
+	public String updateGetcomment(@PathVariable int commentSq , Model model) {
+		CommentEntity comment = csi.commentDetail(commentSq);
+		
+		model.addAttribute("comment", comment);
+		return "pages/fo/boardQnA/updateForm";
+	}
+	
+	@PostMapping("/comment/{commentSq}")
+	public String updateComment(CommentEntity commentEntity) {
+		
+		csi.CommentUptate(commentEntity);
+		return "redirect:{commentSq}";
+		
+	}
+	
+	
+	
 	
 	@PostMapping("/comments")
+	@ResponseBody
 	public void insertComment(@RequestBody CommentEntity commentEntity) {
 		
 		
